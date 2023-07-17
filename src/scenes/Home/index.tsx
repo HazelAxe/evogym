f86c5d8e38b83b9fe73806c3a252f3f6a96749a5
Hazelaxe <17707238960@163.com>
2023-07-17 20:48:26 +0800

    update

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,7 +1,6 @@
+import useMediaQuery from '@/hooks/useMediaQuery'
 import { SelectedPage } from '@/shared/types'
 import ActionButton from '@/shared/ActionButton'
-import useMediaQuery from '@/hooks/useMediaQuery'
-
 import HomePageText from '@/assets/HomePageText.png'
 import HomePageGraphic from '@/assets/HomePageGraphic.png'
 import SponsorRedBull from '@/assets/SponsorRedBull.png'
@@ -9,77 +8,77 @@ import SponsorForbes from '@/assets/SponsorForbes.png'
 import SponsorFortune from '@/assets/SponsorFortune.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { motion } from 'framer-motion'
-
+// NEW
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
 }
 
-const Home = (setSelectedPage: Props) => {
-  const isAboveMediumScreen = useMediaQuery('(min-width:1060px)')
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery('(min-width:1060px)')
 
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      {/* Images and Main Header */}
-      <motion.div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6" onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
-        {/* Main Header */}
+      {/* IMAGE AND MAIN HEADER */}
+      <motion.div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6" onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
+        {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
-          {/* Headings */}
+          {/* HEADINGS */}
           <motion.div
             className="md:-mt-20"
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }} //只触发1次，看见多少div就触发
-            transition={{ duration: 0.5 }} //动画时长
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
             variants={{
-              hidden: { opacity: 0, x: -100 },
+              hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
             }}
           >
-            {/*      父元素为相对的 */}
             <div className="relative">
-              {/*    图片为Absolute */}
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
-                <img src={HomePageText} alt="home-page-text" />
+                <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
 
-            <p className="mt-8 text-sm">To your dream body goals,World class studios to get the body shape that you dream of...You just need to work harder,want it more</p>
+            <p className="mt-8 text-sm">Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the Body Shapes That you Dream of.. Get Your Dream Body Now.</p>
           </motion.div>
-          {/* Actions */}
+
+          {/* ACTIONS */}
           <motion.div
             className="mt-8 flex items-center gap-8"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.3, duration: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
             variants={{
-              hidden: { opacity: 0, x: -100 },
+              hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
             }}
           >
-            <ActionButton setSelectedPage={setSelectedPage}>
-              {/*  */}
-              Join Now
-            </ActionButton>
-
+            <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
             <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500" onClick={() => setSelectedPage(SelectedPage.ContactUs)} href={`#${SelectedPage.ContactUs}`}>
               <p>Learn More</p>
             </AnchorLink>
           </motion.div>
         </div>
-        {/* Images */}
-        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
-          <img src={HomePageGraphic} alt="home-page-graphic" />
+
+        {/* IMAGE */}
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </motion.div>
-      {/* Sponsors */}
-      {isAboveMediumScreen && (
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img src={SponsorRedBull} alt="redbull-sponsor" />
-              <img src={SponsorForbes} alt="forbes-sponsor" />
-              <img src={SponsorFortune} alt="fortune-sponsor" />
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
             </div>
           </div>
         </div>
